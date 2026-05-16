91c5bf5abc7d42a9e06f469c0f972251784058cb
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2026-05-16 16:17:31 +0900

    update

--- 잡은 물고기의 평균 길이 구하기.sql
+++ 잡은 물고기의 평균 길이 구하기.sql
@@ -13,3 +13,6 @@ TIME	DATE	FALSE
 평균 길이를 나타내는 컬럼 명은 AVERAGE_LENGTH로 해주세요.
 평균 길이는 소수점 3째자리에서 반올림하며, 10cm 이하의 물고기들은 10cm 로 취급하여 평균 길이를 구해주세요.
 */
+
+SELECT ROUND(AVG(IFNULL(LENGTH, 10)), 2) AS AVERAGE_LENGTH
+FROM FISH_INFO;
